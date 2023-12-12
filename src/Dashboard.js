e71817dad6a72472de5edfc46222dde62a38eccb
izassb <izabelly.0000830234@unicap.br>
2023-12-12 14:28:09 -0300

    finish

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,8 +2,29 @@ import React, { useState } from 'react';
 import { Layout, Row, Col, Card, Table, Input, Form, Button, Upload, message } from 'antd';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import axios from 'axios';
+import {
+  regionCodes,
+  ufCodes,
+  dependencyCodes,
+  locationCodes,
+  specialLocationCodes,
+  situationCodes,
+  educationSecretariatCodes,
+  publicSecurityCodes,
+  healthSecretariatCodes,
+  otherPublicOrganizationCodes
+} from './mapeamentos';
+import './dashboard.css';
 
 const { Content } = Layout;
+
+const mapCodeToValue = (value) => {
+  return value !== undefined ? value : 'N/A';
+};
+
+const CodeValue = (code, mapping) => {
+  return mapping[code] || 'N/A';
+}
 
 const Dashboard = () => {
   const [filteredData, setFilteredData] = useState([]);
@@ -44,60 +65,235 @@ const Dashboard = () => {
     return false;
   };
 
-  const columns = Object.keys(csvData[0] || {}).map((key) => ({ title: key, dataIndex: key, key }));
+  const columns = Object.keys(csvData[0] || {}).map((key) => {
+    let title = key;
+    let dataIndex = key;
+
+    if (key === 'coRegiao') {
+      title = 'Região';
+      dataIndex = 'coRegiao';
+    } else if (key === 'coUf') {
+      title = 'UF';
+      dataIndex = 'coUf';
+    } else if (key === 'tpDependencia') {
+      title = 'Dependência Administrativa';
+      dataIndex = 'tpDependencia';
+    } else if (key === 'tpLocalizacao') {
+      title = 'Localização';
+      dataIndex = 'tpLocalizacao';
+    } else if (key === 'tpLocalizacaoDiferenciada') {
+      title = 'Localização Diferenciada';
+      dataIndex = 'tpLocalizacaoDiferenciada';
+    } else if (key === 'tpSituacaoFuncionamento') {
+      title = 'Situação Funcionamento';
+      dataIndex = 'tpSituacaoFuncionamento';
+    } else if (key === 'inVinculoSecretariaEducacao') {
+      title = 'Vinculo Secretaria Educação';
+      dataIndex = 'inVinculoSecretariaEducacao';
+    } else if (key === 'inVinculoSegurancaPublica') {
+      title = 'Vinculo Segurança Pública';
+      dataIndex = 'inVinculoSegurancaPublica';
+    } else if (key === 'inVinculoSecretariaSaude') {
+      title = 'Vinculo Secretaria Saúde';
+      dataIndex = 'inVinculoSecretariaSaude';
+    } else if (key === 'inVinculoOutroOrgao') {
+      title = 'Vinculo Outro Orgão';
+      dataIndex = 'inVinculoOutroOrgao';
+    }
+
+    return { title, dataIndex, key };
+  });
 
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Content style={{ margin: '16px' }}>
-        <h1>Dashboard</h1>
+    <Layout style={{ height: '100%' }}>
+      <Content style={{ margin: '20px' }}>
+        <h1>Dashboard Censo Escolar</h1>
         <Upload
           beforeUpload={handleUpload}
           fileList={fileList}
           showUploadList={false}
         >
-          <Button style={{ marginBottom: '30px'}}>
+          <Button className="upload-button" style={{ marginBottom: '30px', fontWeight:'bold'}}>
              Upload CSV
           </Button>
         </Upload>
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="Region Code">{/* Seus dados aqui */}</Card>
-          </Col>
-          <Col span={8}>
-            <Card title="UF Code">{/* Seus dados aqui */}</Card>
-          </Col>
-          <Col span={8}>
-            <Card title="County Code">{/* Seus dados aqui */}</Card>
-          </Col>
-        </Row>
 
         <Row gutter={16} style={{ marginTop: 16 }}>
           <Col span={12}>
-            <Card title="Filtered Data">
-              <Form onFinish={onFinish} layout="inline">
-                <Form.Item name="regionSearch">
-                  <Input placeholder="Search by Region" />
-                </Form.Item>
-                <Form.Item>
-                  <Button type="primary" htmlType="submit">
-                    Search
-                  </Button>
-                </Form.Item>
-              </Form>
-              <Table dataSource={filteredData} columns={columns} />
-            </Card>
-          </Col>
-          <Col span={12}>
-            <Card title="Region Distribution">
-              <BarChart width={400} height={300} data={csvData}>
+            <Card title="Distribuição de Escolas por Localização" style={{ width: '100%' }}>
+              <BarChart width={400} height={300} data={filteredData}>
                 <XAxis dataKey="noEntidade" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                {columns.map((column, index) => (
-                  <Bar key={index} dataKey={column.dataIndex} fill="#8884d8" />
-                ))}
+                <Bar dataKey="tpLocalizacao" fill="#800080" name="Escolas Urbanas" stackId="location" />
+                <Bar dataKey="tpLocalizacaoDiferenciada" fill="#00BFFF" name="Escolas Rurais" stackId="location" />
               </BarChart>
+            </Card>
+          </Col>
+
+          <Col span={12}>
+            <Card title="Distribuição de Escolas por Dependência Administrativa" style={{ width: '100%' }}>
+              <BarChart width={400} height={300} data={filteredData}>
+                <XAxis dataKey="noEntidade" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="tpDependencia" fill="#FFD700" name="Escolas Federais" stackId="dependency" />
+                <Bar dataKey="tpDependencia" fill="#32CD32" name="Escolas Estaduais" stackId="dependency" />
+                <Bar dataKey="tpDependencia" fill="#FF6347" name="Escolas Municipais" stackId="dependency" />
+                <Bar dataKey="tpDependencia" fill="#8A2BE2" name="Escolas Privadas" stackId="dependency" />
+              </BarChart>
+            </Card>
+          </Col>
+        </Row>
+        
+
+        <Row gutter={16} style={{ marginTop: 16 }}>
+          <Col span={50}>
+            <Card title="Data" style={{width: '100%'}}>
+              <Table dataSource={filteredData} columns={columns}  style={{width: '100%'}}/>
+            </Card>
+          </Col>
+
+        </Row>
+
+        <br />
+        <br />
+
+        <Row gutter={16} style={{ }}>
+          <Col span={1.5}>
+            <Card title="ID">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.id)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={5}>
+            <Card title="Instituição">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.noEntidade)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={2}>
+            <Card title="Região">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.coRegiao, regionCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={2}>
+            <Card title="Estado">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.coUf, ufCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+
+          <Col span={4}>
+            <Card title="Dependência Administrativa">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.tpDependencia, dependencyCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+
+          <Col span={4}>
+            <Card title="Localização">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.tpLocalizacao, locationCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+
+          <Col span={5}>
+            <Card title="Localização diferenciada da escola">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.tpLocalizacaoDiferenciada, specialLocationCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+        </Row>
+
+<br />
+
+      <Row gutter={16} style={{  }}>
+      <Col span={3}>
+            <Card title="Código Município">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.coMunicipio)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card title="Código Mesorregião">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.coMesorregiao)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card title="Código Microrregião">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.coMicrorregiao)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={3}>
+            <Card title="Código Distrito">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.coDistrito)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={3}>
+            <Card title="Código Instituição">
+              {filteredData.map((item, index) => (
+                <p key={index}>{mapCodeToValue(item.coEntidade)}</p>
+              ))}
+            </Card>
+          </Col>
+
+          <Col span={4}>
+            <Card title="Situação de funcionamento">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.tpSituacaoFuncionamento, situationCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+  
+        </Row>
+
+        <br />
+
+        <Row gutter={16} style={{  }}>
+          <Col span={5}>
+            <Card title="Vínculo Secretaria Educação">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.inVinculoSecretariaEducacao, educationSecretariatCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card title="Vínculo Segurança Pública">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.inVinculoSegurancaPublica, publicSecurityCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card title="Vínculo Secretaria Saúde">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.inVinculoSecretariaSaude, healthSecretariatCodes)}</p>
+              ))}
+            </Card>
+          </Col>
+          <Col span={4}>
+            <Card title="Vínculo Outros Orgãos">
+              {filteredData.map((item, index) => (
+                <p key={index}>{CodeValue(item.inVinculoOutroOrgao, otherPublicOrganizationCodes)}</p>
+              ))}
             </Card>
           </Col>
         </Row>
---
+++ src/mapeamentos.js
@@ -0,0 +1,87 @@
+export const regionCodes = {
+  1: 'Norte',
+  2: 'Nordeste',
+  3: 'Sudeste',
+  4: 'Sul',
+  5: 'Centro-Oeste',
+};
+
+export const ufCodes = {
+  11: 'Rondônia',
+  12: 'Acre',
+  13: 'Amazonas',
+  14: 'Roraima',
+  15: 'Pará',
+  16: 'Amapá',
+  17: 'Tocantins',
+  21: 'Maranhão',
+  22: 'Piauí',
+  23: 'Ceará',
+  24: 'Rio Grande do Norte',
+  25: 'Paraíba',
+  26: 'Pernambuco',
+  27: 'Alagoas',
+  28: 'Sergipe',
+  29: 'Bahia',
+  31: 'Minas Gerais',
+  32: 'Espírito Santo',
+  33: 'Rio de Janeiro',
+  34: 'Mato Grosso do Sul',
+  35: 'São Paulo',
+  41: 'Paraná',
+  42: 'Santa Catarina',
+  43: 'Rio Grande do Sul',
+  51: 'Mato Grosso',
+  52: 'Goiás',
+  53: 'Distrito Federal',
+};
+
+export const dependencyCodes = {
+  1: 'Federal',
+  2: 'Estadual',
+  3: 'Municipal',
+  4: 'Privada',
+};
+
+export const locationCodes = {
+  1: 'Urbana',
+  2: 'Rural',
+};
+
+export const specialLocationCodes = {
+  0: 'Não está em área de localização diferenciada',
+  1: 'Área de assentamento',
+  2: 'Terra indígena',
+  3: 'Área onde se localiza comunidade remanescente de quilombos',
+};
+
+export const situationCodes = {
+  1: 'Em Atividade',
+  2: 'Paralisada',
+  3: 'Extinta (ano do Censo)',
+  4: 'Extinta em Anos Anteriores',
+};
+
+export const educationSecretariatCodes = {
+  0: 'Não',
+  1: 'Sim',
+  2: 'Não aplicável para escolas privadas',
+};
+
+export const publicSecurityCodes = {
+  0: 'Não',
+  1: 'Sim',
+  2: 'Não aplicável para escolas privadas',
+};
+
+export const healthSecretariatCodes = {
+  0: 'Não',
+  1: 'Sim',
+  2: 'Não aplicável para escolas privadas',
+};
+
+export const otherPublicOrganizationCodes = {
+  0: 'Não',
+  1: 'Sim',
+  2: 'Não aplicável para escolas privadas',
+};
